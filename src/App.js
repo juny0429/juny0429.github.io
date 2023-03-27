e84f474419f228ff3578e8380e5dec8ec13d9184
박준영 <duqrlqkrwnl@gmail.com>
2023-03-27 21:13:56 +0900

    git page 설정 테스트중
    commit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        {/* <Route path="/" element={<Main/>} /> */}
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Main/>} />
+        <Route path="home" element={<Home/>}/>
         <Route path="/" element={<Layout/>}>
           <Route path="/skills" element={<Skills/>}/> 
           <Route path="/about" element={<About/>}/> 
-          <Route path="contact" element={<Contact/>}/> 
+          <Route path="/contact" element={<Contact/>}/> 
         </Route>
       </Routes>
     </BrowserRouter>
